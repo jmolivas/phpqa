9cccdc8f7774fd87b409ccf6cec660e9eb879438
Jose A Duarte <jose.duarte@bookit.com>
2015-08-05 11:26:08 -0400

    Make sure that --files handles folders relative to the current working directory

--- src/Command/AnalyzeCommand.php
+++ src/Command/AnalyzeCommand.php
@@ -257,7 +257,7 @@ class AnalyzeCommand extends Command
         }
 
         foreach ($files as $file) {
-            if (!preg_match($this->needle, $file) && !is_dir(realpath($this->directory.$file))) {
+            if (!preg_match($this->needle, $file) && !is_dir($config->getApplicationDirectory().$file)) {
                 continue;
             }
 
